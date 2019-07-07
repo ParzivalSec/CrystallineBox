9a6d06d38e491cd409673d476a226fea219c635c
ParzivalSec <lukas.vogl12@gmail.com>
2019-07-07 22:31:53 +0200

    Changed CrystallineBox builds and added samples
    - Changec CrystallineBox to compile as static lib
    - Added a samples folder
    - Created the basis for the win32 sample

--- premake5.lua
+++ premake5.lua
@@ -7,15 +7,89 @@ workspace "CrystallineBox"
     configurations { "Debug", "FastDebug", "Live++", "Release", "Retail" }
     platforms { "x64" }
 
+project "CrystallineSampleWin32"
+	targetname "CrystallineSampleWin32"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++11"
+	systemversion "latest"
+	entrypoint "mainCRTStartup"
+	targetdir(BUILD_DIR .. "/samples/sample_win32/%{cfg.buildcfg}")
+	debugdir("./samples/sample_win32")
+	
+	files { "samples/sample_win32/**.h", "samples/sample_win32/**.cpp" }
+	
+	links { "CrystallineBox" }
+	
+    flags "FatalWarnings" -- All warnings on.
+
+    filter "platforms:x64"
+        architecture "x64"
+
+		defines { "PLATFORM_WIN32" }
+
+		filter { "configurations:Debug" }
+			defines { "DEBUG" }
+			symbols "Full"
+			optimize "Off"
+			
+		filter { "configurations:FastDebug" }
+			defines { "DEBUG" }
+			symbols "Full"
+			optimize "On"
+
+		filter "configurations:Release"
+			defines { "RELEASE", "NDEBUG" }
+			symbols "Full"
+			optimize "Full"
+			
+		filter "configurations:Retail"
+			defines { "RETAIL", "NDEBUG" }
+			symbols "Off"
+			optimize "Full"
+	
+	local VULKAN_SDK_DIR = os.getenv("VULKAN_SDK_DIR") -- path to the LunarG Vulkan SDK
+
+	if VULKAN_SDK_DIR ~= nil then
+		includedirs { VULKAN_SDK_DIR .. "Include\\" }
+		libdirs { VULKAN_SDK_DIR .. "Lib\\" }
+		links { "vulkan-1" }
+	else
+		error("Could not find the path to the vulkan sdk - please check if the VULKAN_SDK_DIR environment variable is set to the correct path!")
+	end
+	
+	local LPP_DIR = os.getenv("LPP_DIR") -- path to the folder where Live++ lives (if this is set we create a new build configuration for a LPP enabled debug build)
+
+	if LPP_DIR ~= nil then
+		---------------------------------------
+		-- Live++ only in debug builds for now
+		---------------------------------------
+		filter { "configurations:Live++" }
+			defines { "DEBUG", "LPP" }
+			symbols "Full"
+			optimize "Off"
+			includedirs { LPP_DIR }
+			
+			functionlevellinking  "On" -- Enable function level linking (wraps functions in own COMDATs and simplifies COMDAT folding)
+			buildoptions { "/Zi /Gw" } -- Enable the creation of .pdb's (not for MSVC's edit and continue) and enable 'Global data optimization' (also own COMDATs for the data) 
+			linkoptions { "/FUNCTIONPADMIN", "/OPT:NOREF", "/OPT:NOICF" }
+			-- /FUNCTIONPADMIN (hotpatchable-image) -> describes how much padding is added in front of functions (x64: 6 byte default)
+			-- /OPT:NOREF -> keep COMDAT's (function and data) which are never referenced (LivePP requ.)
+			-- /OPT:NOICF -> disable similar COMDAT folding for MSVC (LivePP requ.)
+			
+		filter {}
+	else
+		error("Could not find the path to the lpp dir!")
+	end
+
+
 project "CrystallineBox"
     targetname "CrystallineBox"
-    kind "ConsoleApp"
+    kind "StaticLib"
     language "C++"
     cppdialect "C++11"
     systemversion "latest" -- to use latest windows SDK (VS17 defaulted to 8.1 SDK in my case)
-    entrypoint "mainCRTStartup"
     targetdir(BUILD_DIR .. "/crystalline_box/%{cfg.buildcfg}")
-    debugdir("./")
 
 ---------------------------
 -- Compiler/Linker settings
@@ -25,25 +99,27 @@ project "CrystallineBox"
     filter "platforms:x64"
         architecture "x64"
 
-    filter { "configurations:Debug" }
-        defines { "DEBUG" }
-        symbols "Full"
-        optimize "Off"
-		
-    filter { "configurations:FastDebug" }
-        defines { "DEBUG" }
-        symbols "Full"
-        optimize "On"
+		defines { "PLATFORM_WIN32" }
 
-    filter "configurations:Release"
-        defines { "RELEASE", "NDEBUG" }
-        symbols "Full"
-        optimize "Full"
+		filter { "configurations:Debug" }
+			defines { "DEBUG" }
+			symbols "Full"
+			optimize "Off"
+			
+		filter { "configurations:FastDebug" }
+			defines { "DEBUG" }
+			symbols "Full"
+			optimize "On"
 
-    filter "configurations:Retail"
-        defines { "RETAIL", "NDEBUG" }
-        symbols "Off"
-        optimize "Full"
+		filter "configurations:Release"
+			defines { "RELEASE", "NDEBUG" }
+			symbols "Full"
+			optimize "Full"
+
+		filter "configurations:Retail"
+			defines { "RETAIL", "NDEBUG" }
+			symbols "Off"
+			optimize "Full"
 
     filter {}
 
@@ -69,30 +145,6 @@ if VULKAN_SDK_DIR ~= nil then
 	links { "vulkan-1" }
 else
 	error("Could not find the path to the vulkan sdk - please check if the VULKAN_SDK_DIR environment variable is set to the correct path!")
-end
-
-local LPP_DIR = os.getenv("LPP_DIR") -- path to the folder where Live++ lives (if this is set we create a new build configuration for a LPP enabled debug build)
-
-if LPP_DIR ~= nil then
-	---------------------------------------
-	-- Live++ only in debug builds for now
-	---------------------------------------
-    filter { "configurations:Live++" }
-        defines { "DEBUG", "LPP" }
-        symbols "Full"
-        optimize "Off"
-		includedirs { LPP_DIR }
-		
-		functionlevellinking  "On" -- Enable function level linking (wraps functions in own COMDATs and simplifies COMDAT folding)
-		buildoptions { "/Zi /Gw" } -- Enable the creation of .pdb's (not for MSVC's edit and continue) and enable 'Global data optimization' (also own COMDATs for the data) 
-		linkoptions { "/FUNCTIONPADMIN", "/OPT:NOREF", "/OPT:NOICF" }
-		-- /FUNCTIONPADMIN (hotpatchable-image) -> describes how much padding is added in front of functions (x64: 6 byte default)
-		-- /OPT:NOREF -> keep COMDAT's (function and data) which are never referenced (LivePP requ.)
-		-- /OPT:NOICF -> disable similar COMDAT folding for MSVC (LivePP requ.)
-		
-	filter {}
-else
-	error("Could not find the path to the lpp dir!")
 end
 
 includedirs
---
+++ samples/sample_win32/main.cpp
@@ -0,0 +1,16 @@
+﻿#include <cstdio>
+
+#include "../../src/graphics/PlatformWindow/PlatformWindow.h"
+#include "../../src/graphics/PlatformWindow/PlatformWindowWindowsImpl.h"
+
+int main(int argc, char** argv)
+{
+	printf("Hello, Crystallinebox!\n");
+
+	cry::PlatformWindow window(L"ASDF", 1920u, 1080u);
+
+
+	printf("Window title: %S\n", window.GetTitle());
+
+	return 0;
+}
